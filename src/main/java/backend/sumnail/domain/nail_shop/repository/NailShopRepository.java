47019acd511dbcbad5194afc0240f31410dca221
hayoun <fishingest@kookmin.ac.kr>
2023-11-30 13:30:00 +0900

    feat: NailShopRepository 생성

---
+++ src/main/java/backend/sumnail/domain/nail_shop/repository/NailShopRepository.java
@@ -0,0 +1,11 @@
+package backend.sumnail.domain.nail_shop.repository;
+
+import backend.sumnail.domain.nail_shop.entity.NailShop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+public interface NailShopRepository extends JpaRepository<NailShop,Long>{
+    List<NailShop> findALL();
+}
