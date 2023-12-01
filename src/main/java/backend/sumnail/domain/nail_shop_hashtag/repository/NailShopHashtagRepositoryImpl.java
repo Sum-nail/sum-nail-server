64cdf482f2aae293e3ffcbd2cb22192816bb3bea
dltjdn <hana40004@naver.com>
2023-12-01 18:19:12 +0900

    feat: NailShopHashtag 리포지토리 추상화

---
+++ src/main/java/backend/sumnail/domain/nail_shop_hashtag/repository/NailShopHashtagRepositoryImpl.java
@@ -0,0 +1,17 @@
+package backend.sumnail.domain.nail_shop_hashtag.repository;
+
+import backend.sumnail.domain.nail_shop_hashtag.entity.NailShopHashtag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class NailShopHashtagRepositoryImpl implements NailShopHashtagRepository {
+    private final NailShopHashtagJpaRepository nailShopHashTagJpaRepository;
+
+    @Override
+    public List<NailShopHashtag> getByNailShopId(Long id) {
+        return nailShopHashTagJpaRepository.findByNailShopId(id);
+    }
+}
