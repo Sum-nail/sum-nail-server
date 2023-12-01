a5e8ad20594b0d888b9fb5d9f6e566da771dc6e1
dltjdn <hana40004@naver.com>
2023-12-02 03:41:05 +0900

    docs: controller 주석 추가

--- src/main/java/backend/sumnail/domain/user/controller/dto/UserController.java
+++ src/main/java/backend/sumnail/domain/user/controller/dto/UserController.java
@@ -23,40 +23,58 @@ public class UserController {
     private final UserService userService;
 
     // TODO 테스트 용 userId 1 번 사용중
+
+    /**
+     * 나의 프로필 조회
+     */
     @GetMapping("profile")
     public ResponseEntity<UserFindResponse> findUser() {
         UserFindResponse response = userService.findUser(1);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    /**
+     * 저장한 네일샵 전체 조회
+     */
     @GetMapping("nail-shops")
     public ResponseEntity<List<UserFindNailShopResponse>> findAllNailShopsUser() {
         List<UserFindNailShopResponse> responses = userService.findAllNailShopsUser(1);
         return ResponseEntity.status(HttpStatus.OK).body(responses);
     }
 
+    /**
+     * 네일샵 저장하기
+     */
     @PostMapping("nail-shops/{nailShopId}")
     public ResponseEntity<Void> saveNailShopUser(@PathVariable("nailShopId") long nailShopId) {
         userService.saveNailShopUser(1, nailShopId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+     * 네일샵 저장 취소하기
+     */
     @DeleteMapping("nail-shops/{nailShopId}")
     public ResponseEntity<Void> deleteNailShopUser(@PathVariable("nailShopId") long nailShopId) {
         userService.deleteNailShopUser(1, nailShopId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-
+    /**
+     * 지하철 역 검색 내역 조회
+     */
     @GetMapping("search-station-history")
-    public ResponseEntity<UserFindSearchStationsResponse> findSearchStationsUser(long userId) {
-        UserFindSearchStationsResponse response = userService.findSearchStationsUser(userId);
+    public ResponseEntity<UserFindSearchStationsResponse> findSearchStationsUser() {
+        UserFindSearchStationsResponse response = userService.findSearchStationsUser(1);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    /**
+     * 지하철 역 검색 기록 전체 삭제
+     */
     @DeleteMapping("search-station-history")
-    public ResponseEntity<Void> deleteSearchStationsUser(long userId) {
-        userService.deleteSearchStationsUser(userId);
+    public ResponseEntity<Void> deleteSearchStationsUser() {
+        userService.deleteSearchStationsUser(1);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
