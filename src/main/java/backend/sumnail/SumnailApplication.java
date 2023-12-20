f6019947dc205f9bdc01066064f999cf20beadae
dltjdn <hana40004@naver.com>
2023-12-21 03:19:57 +0900

    chore: feign 의존성 추가

--- src/main/java/backend/sumnail/SumnailApplication.java
+++ src/main/java/backend/sumnail/SumnailApplication.java
@@ -2,8 +2,10 @@ package backend.sumnail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SumnailApplication {
 
     public static void main(String[] args) {
