7a566c9c9867256d248c3c3b4d71cf648de9250f
hayoun <fishingest@kookmin.ac.kr>
2023-12-26 02:24:51 +0900

    feat: 검색 기록 저장 dto 구현

---
+++ src/main/java/backend/sumnail/domain/user/controller/dto/request/RecentSearchSaveRequest.java
@@ -0,0 +1,10 @@
+package backend.sumnail.domain.user.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class RecentSearchSaveRequest {
+    private String stationName;
+}
