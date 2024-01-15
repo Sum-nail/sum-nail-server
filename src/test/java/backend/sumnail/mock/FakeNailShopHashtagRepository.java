e86337ff6922e451fc4251fc133fe7e662306f7b
hayoun <fishingest@kookmin.ac.kr>
2024-01-16 02:29:20 +0900

    test: FakeNailShop, NailShopHashtag Repository 구현

--- src/test/java/backend/sumnail/mock/FakeNailShopHashtagRepository.java
+++ src/test/java/backend/sumnail/mock/FakeNailShopHashtagRepository.java
@@ -1,2 +1,36 @@
-package backend.sumnail.mock;public class FakeNailShopHashtagRepository {
+package backend.sumnail.mock;
+
+import backend.sumnail.domain.hashtag.entity.Hashtag;
+import backend.sumnail.domain.nail_shop_hashtag.entity.NailShopHashtag;
+import backend.sumnail.domain.nail_shop_hashtag.repository.NailShopHashtagRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class FakeNailShopHashtagRepository implements NailShopHashtagRepository {
+    private final Long id=new AtomicLong().getAndIncrement();
+    private final List<NailShopHashtag> data=new ArrayList<>();
+
+    @Override
+    public List<NailShopHashtag> getByNailShopId(Long id) {
+        return data.stream()
+                .filter(item->item.getNailShop().getId().equals(id))
+                .toList();
+    }
+
+    @Override
+    public void save(NailShopHashtag nailShopHashtag) {
+        if(nailShopHashtag.getId()==null||nailShopHashtag.getId()==0)
+        {
+            NailShopHashtag newNailShopHashtag=NailShopHashtag.builder()
+                    .nailShop(nailShopHashtag.getNailShop())
+                    .hashtag(nailShopHashtag.getHashtag())
+                    .build();
+        }
+        else{
+            data.removeIf(it->it.getId()==nailShopHashtag.getId());
+        }
+        data.add(nailShopHashtag);
+    }
 }
--- src/test/java/backend/sumnail/mock/FakeNailShopRepository.java
+++ src/test/java/backend/sumnail/mock/FakeNailShopRepository.java
@@ -1,2 +1,56 @@
-package backend.sumnail.mock;public class FakeNailShopRepository {
+package backend.sumnail.mock;
+
+import backend.sumnail.domain.nail_shop.entity.NailShop;
+import backend.sumnail.domain.nail_shop.repository.NailShopRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class FakeNailShopRepository implements NailShopRepository {
+
+    private final Long id=new AtomicLong().getAndIncrement();
+    private final List<NailShop> data=new ArrayList<>();
+    @Override
+    public List<NailShop> findAll() {
+        return data;
+    }
+
+    @Override
+    public List<NailShop> findNailShopsByHashtagAndStation(String stationName, String hashtagName) {
+        return data.stream()
+                .filter(item->item.getStations().stream().anyMatch(station->station.getStation().getStationName().contains(stationName))
+                       && item.getHashtags().stream().anyMatch(hashtag->hashtag.getHashtag().getHashtagName().contains(hashtagName)))
+                .toList();
+    }
+
+    @Override
+    public Optional<NailShop> findById(Long id) {
+        return data.stream()
+                .filter(item->item.getId().equals(id))
+                .findAny();
+    }
+
+    @Override
+    public NailShop getById(long id) {
+        return data.stream()
+                .filter(item->item.getId().equals(id))
+                .findAny()
+                .orElseThrow();
+    }
+
+    @Override
+    public List<NailShop> findNailShopByHashtag(String hashtagName) {
+        return data.stream()
+                .filter(item->item.getHashtags().stream().anyMatch(hashtag->hashtag.getHashtag().getHashtagName().equals(hashtagName)))
+                .toList();
+    }
+
+    @Override
+    public List<NailShop> findNailShopByStation(String stationName) {
+        return data.stream()
+                .filter(item->item.getStations().stream().anyMatch(station->station.getStation().getStationName().equals(stationName)))
+                .toList();
+    }
 }
