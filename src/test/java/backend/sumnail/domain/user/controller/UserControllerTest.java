7904fbb57b99ea22383228794b5b3bce6a4fbea0
dltjdn <hana40004@naver.com>
2024-01-17 01:48:59 +0900

    test: 네일샵 저장 api 테스트 추가

--- src/test/java/backend/sumnail/domain/user/controller/UserControllerTest.java
+++ src/test/java/backend/sumnail/domain/user/controller/UserControllerTest.java
@@ -1,12 +1,17 @@
 package backend.sumnail.domain.user.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import backend.sumnail.domain.user.entity.User;
+import backend.sumnail.domain.user_nail_shop.entity.UserNailShop;
+import backend.sumnail.domain.user_nail_shop.repository.UserNailShopRepository;
 import backend.sumnail.global.config.jwt.PrincipalDetails;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -28,6 +33,8 @@ import org.springframework.test.web.servlet.MockMvc;
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private UserNailShopRepository userNailShopRepository;
 
     @Test
     void 자신의_프로필을_조회_할_수_있다() throws Exception{
@@ -83,7 +90,7 @@ class UserControllerTest {
                 .andExpect(jsonPath("$[0].nailShopName").value("썸네일네일샵"))
                 .andExpect(jsonPath("$[0].location").value("서울시 중구"))
                 .andExpect(jsonPath("$[0].titleImage").value("http://zoom.us?g=1"))
-                .andExpect(jsonPath("$[0].hashtags", containsInAnyOrder("심플한", "화한")));
+                .andExpect(jsonPath("$[0].hashtags", containsInAnyOrder("심플한", "화려한")));
     }
 
     @Test
@@ -100,6 +107,42 @@ class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$").isEmpty());
+    }
+
+
+    @Test
+    void 네일샵을_저장할_수_있다() throws Exception{
+        //given
+        User user = User.builder()
+                .id(2L)
+                .build();
+        PrincipalDetails principalDetails = new PrincipalDetails(user);
+        //when
+        //then
+        mockMvc.perform(post("/v1/user/nail-shops/1")
+                        .with(SecurityMockMvcRequestPostProcessors.user(principalDetails)))
+                .andExpect(status().isCreated());
+
+        List<UserNailShop> userNailShops = userNailShopRepository.findByUserId(2);
+        assertThat(userNailShops.size()).isEqualTo(1);
+        assertThat(userNailShops.get(0).getUser().getId()).isEqualTo(2);
+        assertThat(userNailShops.get(0).getNailShop().getId()).isEqualTo(1);
+    }
+
+    @Test
+    void 네일샵을_중복해서_저장하면_409_응답을_받는다() throws Exception {
+        //given
+        User user = User.builder()
+                .id(1L)
+                .build();
+        PrincipalDetails principalDetails = new PrincipalDetails(user);
+        //when
+        //then
+        mockMvc.perform(post("/v1/user/nail-shops/1")
+                        .with(SecurityMockMvcRequestPostProcessors.user(principalDetails)))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.status").value("CONFLICT"))
+                .andExpect(jsonPath("$.error").value("이미 저장한 네일샵입니다."));
     }
 
 
