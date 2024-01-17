10f4e2792b37df6e289acedc83f9ec3b5827c2c7
dltjdn <hana40004@naver.com>
2024-01-18 01:01:40 +0900

    test: UserFindNailShopResponse 생성 테스트

---
+++ src/test/java/backend/sumnail/domain/user/controller/dto/response/UserFindNailShopResponseTest.java
@@ -0,0 +1,50 @@
+package backend.sumnail.domain.user.controller.dto.response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import backend.sumnail.domain.hashtag.entity.Hashtag;
+import backend.sumnail.domain.nail_shop.controller.dto.NailShopFindSavedDto;
+import backend.sumnail.domain.nail_shop.entity.NailShop;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserFindNailShopResponseTest {
+
+    @Test
+    @DisplayName("NailShopFindSavedDto 객체로 응답을 생성할 수 있다.")
+    public void fromTest(){
+        //given
+        NailShop nailShop = NailShop.builder()
+                .id(1L)
+                .location("서울시 중구")
+                .name("썸네일네일샵")
+                .titleImage("http://zoom.us?g=1")
+                .build();
+
+        List<Hashtag> hashtags = new ArrayList<>();
+        Hashtag hashtag = Hashtag.builder()
+                .id(1L)
+                .hashtagName("심플한")
+                .build();
+        hashtags.add(hashtag);
+
+        NailShopFindSavedDto nailShopFindSavedDto = NailShopFindSavedDto.builder()
+                .nailShop(nailShop)
+                .hashtags(hashtags)
+                .build();
+
+        //when
+        UserFindNailShopResponse userFindNailShopResponse = UserFindNailShopResponse.from(nailShopFindSavedDto);
+
+        //then
+        assertThat(userFindNailShopResponse.getNailShopId()).isEqualTo(1L);
+        assertThat(userFindNailShopResponse.getNailShopName()).isEqualTo("썸네일네일샵");
+        assertThat(userFindNailShopResponse.getLocation()).isEqualTo("서울시 중구");
+        assertThat(userFindNailShopResponse.getTitleImage()).isEqualTo("http://zoom.us?g=1");
+        assertThat(userFindNailShopResponse.getHashtags().get(0)).isEqualTo("심플한");
+    }
+
+
+}
--- src/test/resources/sql/user-controller-test-data.sql
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -4,7 +4,7 @@ insert into `users` (`user_id`, `email`,`name`,`profile_image`)
 values( 2, 'abc@yahoo.edu', '썸네일2', 'https://guardian.co.uk/one');
 
 insert into nail_shop (nail_shop_id, map_lat, map_lng, employee_num, monthly_nail_maximum_price, monthly_nail_minimum_price,business_hour, detail_images, location, monthly_nail_instagram_link, nail_shop_name, naver_map_link, reservation_table, street_address, title_image)
-values(1,'41.4019163136','172.669129728',9,14428, 28026, '1:30 PM', 'http://facebook.com/sub?client=g', '서울시 중구','http://cnn.com/fr?q=test','썸네일네일샵','https://pinterest.com/en-ca?client=g','ligula. Nullam enim.', '328-2245 Tincidunt. Ave','http://zoom.us?g=1');
+values(1, 41.4019163136, 172.669129728, 9, 14428, 28026, '1:30 PM', 'http://facebook.com/sub?client=g', '서울시 중구','http://cnn.com/fr?q=test','썸네일네일샵','https://pinterest.com/en-ca?client=g','ligula. Nullam enim.', '328-2245 Tincidunt. Ave','http://zoom.us?g=1');
 
 insert into user_nail_shop (user_id, nail_shop_id)
 values(1, 1);
