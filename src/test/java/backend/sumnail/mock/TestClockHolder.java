43db85109900278896641d66a58635aee00e0326
dltjdn <hana40004@naver.com>
2024-01-18 16:02:47 +0900

    test: localDateTime 형 변환

--- src/test/java/backend/sumnail/mock/TestClockHolder.java
+++ src/test/java/backend/sumnail/mock/TestClockHolder.java
@@ -2,11 +2,15 @@ package backend.sumnail.mock;
 
 import backend.sumnail.domain.common.service.port.ClockHolder;
 import java.time.LocalDateTime;
-import lombok.RequiredArgsConstructor;
+import java.time.format.DateTimeFormatter;
 
-@RequiredArgsConstructor
 public class TestClockHolder implements ClockHolder {
     private final LocalDateTime mills;
+
+    public TestClockHolder(String dateTimeString) {
+        this.mills = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));
+    }
+
     @Override
     public LocalDateTime millis() {
         return mills;
