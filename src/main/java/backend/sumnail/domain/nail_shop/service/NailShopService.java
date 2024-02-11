5f7c7596053cb6d64636b5c55e2df3a494bd8a6f
dltjdn <hana40004@naver.com>
2024-02-11 20:14:01 +0900

    feat: 네일샵 전체 조회 캐시 적용

--- src/main/java/backend/sumnail/domain/nail_shop/service/NailShopService.java
+++ src/main/java/backend/sumnail/domain/nail_shop/service/NailShopService.java
@@ -3,14 +3,18 @@ package backend.sumnail.domain.nail_shop.service;
 import backend.sumnail.domain.hashtag.entity.Hashtag;
 import backend.sumnail.domain.hashtag.service.HashtagService;
 import backend.sumnail.domain.nail_shop.controller.dto.NailShopFindSavedDto;
+import backend.sumnail.domain.nail_shop.controller.dto.NailShopFindDto;
 import backend.sumnail.domain.nail_shop.controller.dto.response.NailShopFindAllResponse;
 import backend.sumnail.domain.nail_shop.controller.dto.response.NailShopFindOneResponse;
 import backend.sumnail.domain.nail_shop.entity.NailShop;
 import backend.sumnail.domain.nail_shop.service.port.NailShopRepository;
+import backend.sumnail.domain.nail_shop_hashtag.entity.NailShopHashtag;
+import backend.sumnail.domain.nail_shop_hashtag.service.port.NailShopHashtagRepository;
 import backend.sumnail.domain.user_nail_shop.entity.UserNailShop;
 import java.util.List;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,17 +26,17 @@ public class NailShopService {
 
     private final NailShopRepository nailShopRepository;
     private final HashtagService hashtagService;
+    private final NailShopHashtagRepository nailShopHashtagRepository;
 
-    public List<NailShopFindAllResponse> findAllShop() {
+    @Cacheable(cacheNames = "NAILSHOP", cacheManager = "cacheManager")
+    public NailShopFindAllResponse findAllShop() {
         List<NailShop> nailShops = nailShopRepository.findAll();
-        List<NailShopFindAllResponse> response = nailShops
-                .stream()
-                .map(NailShopFindAllResponse::from)
-                .toList();
-        return response;
+
+        return toNailShopFindAllResponse(nailShops);
     }
 
-    public List<NailShopFindAllResponse> searchNailShop(String stationName, String hashtagName) {
+
+    public NailShopFindAllResponse searchNailShop(String stationName, String hashtagName) {
         List<NailShop> nailShops;
         if (stationName.isEmpty() && hashtagName.isEmpty()) {
             nailShops = nailShopRepository.findAll();
@@ -43,11 +47,8 @@ public class NailShopService {
         } else {
             nailShops = nailShopRepository.findNailShopsByHashtagAndStation(stationName, hashtagName);
         }
-        List<NailShopFindAllResponse> response = nailShops
-                .stream()
-                .map(NailShopFindAllResponse::from)
-                .toList();
-        return response;
+
+        return toNailShopFindAllResponse(nailShops);
     }
 
     public NailShopFindOneResponse findNailShopById(Long id) {
@@ -64,5 +65,18 @@ public class NailShopService {
 
     public NailShop getById(long nailShopId) {
         return nailShopRepository.getById(nailShopId);
+    }
+
+    private NailShopFindAllResponse toNailShopFindAllResponse(List<NailShop> nailShops) {
+        List<NailShopFindDto> response = nailShops.stream()
+                .map(nailShop -> {
+                    List<String> hashtags = nailShopHashtagRepository.getByNailShopId(nailShop.getId()).stream()
+                            .map(NailShopHashtag::getHashtag)
+                            .map(Hashtag::getHashtagName)
+                            .toList();
+                    return NailShopFindDto.from(nailShop, hashtags);
+                })
+                .toList();
+        return NailShopFindAllResponse.from(response);
     }
 }
